e057ecc3f4fb9933959f3e82ca9870c6d89d83ca
Ravindu Thaveesha <thaveesha.ravindu2@gmail.com>
2023-02-01 23:25:59 +0800

    update test cases

--- test.php
+++ test.php
@@ -15,10 +15,13 @@ function assertWrapOutput($string, $length, $expectedOutput) {
     }
 }
 
-# test cases reference: https://www.php.net/manual/en/function.wordwrap.php
+# test cases reference: 
+# 1. https://www.php.net/manual/en/function.wordwrap.php
+# 2. https://www.w3schools.com/php/func_string_wordwrap.asp
 assertWrapOutput("", 10, "Error: Input string cannot be empty.");
 assertWrapOutput("The quick brown fox jumped over the lazy dog.", 20, "The quick brown fox\njumped over the lazy\ndog.");
-assertWrapOutput("Lengthofthisistwenty", 20, "Lengthofthisistwenty");
 assertWrapOutput("A very long woooooooooooord.", 8, "A very\nlong\nwooooooo\nooooord.");
 assertWrapOutput("A very long woooooooooooooooooord. and something", 8, "A very\nlong\nwooooooo\noooooooo\nooord.\nand\nsomethin\ng");
+assertWrapOutput("An example of a long word is: Supercalifragulistic", 15, "An example of a\nlong word is:\nSupercalifragul\nistic");
+assertWrapOutput("Lengthofthisistwenty", 20, "Lengthofthisistwenty");
 assertWrapOutput("nospacesyetverylongword", 8, "nospaces\nyetveryl\nongword");
